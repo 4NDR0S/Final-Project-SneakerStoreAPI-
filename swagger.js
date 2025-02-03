e589bd0d422f8ca29147aeff604cf462e4403b16
Felix <fguzmanceri@gmail.com>
2025-02-03 13:40:21 -0400

    Adding the Swagger for all API documentations

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Sneaker Store API',
+            version: '1.0.0',
+            description: 'API documentation for the Sneaker Store project',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = swaggerDocs;
