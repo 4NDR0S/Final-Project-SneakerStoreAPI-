3bec2e386f0054b2a288225ed1ba3c5c78c9c50c
Felix <fguzmanceri@gmail.com>
2025-02-03 17:56:05 -0400

    update on database.js

--- data/database.js
+++ data/database.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const mongoose = require('mongoose');
+
 const MongoClient = require('mongodb').MongoClient;
 
 let database;
+
+// Import models
+const User = require('../models/user');
+const Sneaker = require('../models/sneaker');
+const Order = require('../models/order');
+const Category = require('../models/category');
 
 const initDb = (callback) => {
     if (database) {
@@ -13,21 +21,25 @@ const initDb = (callback) => {
     MongoClient.connect(process.env.MONGODB_URL)
         .then((client) => {
             database = client;
-         callback(null, database);
+            callback(null, database);
         })
         .catch((err) => {
             callback(err);
-        })
+        });
 };
 
 const getDatabase = () => {
     if (!database) {
-        throw Error('Database not initialized')
+        throw Error('Database not initialized');
     }
     return database;
-}
+};
 
 module.exports = {
     initDb,
-    getDatabase
+    getDatabase,
+    User,
+    Sneaker,
+    Order,
+    Category
 };
