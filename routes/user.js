8f1474204ac3379cccbf27d5c573e27357796e1c
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2025-02-04 13:48:27 -0400

    updating user.js

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
-
 /**
  * @swagger
  * tags:
@@ -19,6 +18,142 @@ const userController = require('../controllers/user');
  *     responses:
  *       200:
  *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: The user ID
+ *                   name:
+ *                     type: string
+ *                     description: The user's name
+ *                   email:
+ *                     type: string
+ *                     description: The user's email
+ *                   address:
+ *                     type: string
+ *                     description: The user's address
+ *                   phone:
+ *                     type: string
+ *                     description: The user's phone number
+ */
+
+/**
+ * @swagger
+ * /api/user:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name
+ *               email:
+ *                 type: string
+ *                 description: The user's email
+ *               address:
+ *                 type: string
+ *                 description: The user's address
+ *               phone:
+ *                 type: string
+ *                 description: The user's phone number
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ */
+
+/**
+ * @swagger
+ * /api/user/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The ID of the user
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A user object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 address:
+ *                   type: string
+ *                 phone:
+ *                   type: string
+ */
+
+/**
+ * @swagger
+ * /api/user/{id}:
+ *   put:
+ *     summary: Update a user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The ID of the user
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *               phone:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ */
+
+/**
+ * @swagger
+ * /api/user/{id}:
+ *   delete:
+ *     summary: Delete a user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The ID of the user to delete
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: User deleted successfully
  */
 
 router.get('/', (req, res) => {
@@ -26,36 +161,24 @@ router.get('/', (req, res) => {
     userController.getAllUsers(req, res);
 });
 
-
-
 router.get('/:id', (req, res) => {
     console.log('getUserById called');
     userController.getUserById(req, res);
 });
-
-
 
 router.post('/', (req, res) => {
     console.log('createUser called');
     userController.createUser(req, res);
 });
 
-
-
 router.put('/:id', (req, res) => {
     console.log('updateUser called');
     userController.updateUser(req, res);
 });
 
-
-
 router.delete('/:id', (req, res) => {
     console.log('deleteUser called');
     userController.deleteUser(req, res);
 });
-
-
-
-
 
 module.exports = router;
